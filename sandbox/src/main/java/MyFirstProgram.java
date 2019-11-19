21a8bcce85097ed580273a68b8488fd0e00f4947
Perfect User <me@PerfectUser.com>
2019-11-20 02:44:00 +0300

    Создан проект sandbox

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,23 +1,13 @@
-package ru.stqa.pft.sandbox;
+/* Name it "Hello.java" 
+ * Compile it by running "javac Hello.java"
+ * Run it by typing "java Hello"
+ */
 
-public class MyFirstProgram {
-    public static void main(String[] args) {
-        hello("HUILO ");
-        hello("Grigory ");
-        hello("Bird ");
-
-        Square s = new Square(5); //создан объект
-        s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-        Rectangular r = new Rectangular(4, 6);
-
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
-    }
-    public static void hello(String somebody) {
-         System.out.println("Putin " + somebody + "PIDOR");
-    }
-
-
+class MyFirstProgram {
+	public static void main(String args[])
+	{
+    	System.out.println("Hello, world!");
+	}
 }
+
+
