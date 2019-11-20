ef53868acca8f78a3cd973dbde8f25985eabad6f
Perfect User <me@PerfectUser.com>
2019-11-20 23:40:32 +0300

    Создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/* Name it "Hello.java"
+ * Compile it by running "javac Hello.java"
+ * Run it by typing "java Hello"
+ */
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+
+            System.out.println("Hello, World!");
+    }
+}
+
+
