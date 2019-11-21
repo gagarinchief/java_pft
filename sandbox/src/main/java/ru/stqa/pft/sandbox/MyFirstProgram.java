2bd659925e27aa3e42982727d5e23a1525ad6506
Perfect User <me@PerfectUser.com>
2019-11-21 04:45:08 +0300

    Math actions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,6 +9,20 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
             System.out.println("Hello, World!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1.0/2);
+        System.out.println(1/2.0);
+
+        System.out.println("2" +  "2");
+        System.out.println("2" +  2);
+        System.out.println(2 +  "2");
+
+        System.out.println(2 + 2 * 2);
+        System.out.println("2 + 2 = " + (2 +2));
     }
 }
 
