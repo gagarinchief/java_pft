57691e0b70b2f48c6cf32db94381c1c84c3cc7a1
Perfect User <me@PerfectUser.com>
2019-11-24 23:19:43 +0300

    Error 1st Test For Barancev

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test; // для использования коротких имен класса
 
 public class SquareTests {
@@ -7,7 +8,7 @@ public class SquareTests {
     @Test //аннотация
     public void testArea() {
         Square s = new Square(5);
-        assert  s.area() == 25; // assert оценить
+        Assert.assertEquals(s.area(), 25.0); // assert оценить
     }
 
 }
