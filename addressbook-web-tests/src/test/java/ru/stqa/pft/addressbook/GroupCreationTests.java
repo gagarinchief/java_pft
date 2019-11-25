248a48e3ca21493ad34f8496222c4243a2501f99
Perfect User <me@PerfectUser.com>
2019-11-26 02:43:57 +0300

    2.3. Fixture оптимизирован код теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -14,14 +14,10 @@ import java.util.concurrent.TimeUnit;
 public class GroupCreationTests {
   private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true) //Аннотация. Подсказка для тестового framework
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -29,6 +25,11 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
