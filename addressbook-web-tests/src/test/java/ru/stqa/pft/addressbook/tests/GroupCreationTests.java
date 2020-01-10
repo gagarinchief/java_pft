d0ef07276fcc079d49b61b83bb4d29bdb43b8679
Perfect User <me@PerfectUser.com>
2020-01-10 14:02:39 -0500

    6.4 DataProvider
    Параметризация тестовых методов.
    Создание раздельного отчета по проведенным тестам.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,22 +1,34 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[]{new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[]{new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         Groups after = app.group().all();
-
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
         assertThat(after.size(), equalTo(before.size() + 1));
@@ -40,7 +52,6 @@ public class GroupCreationTests extends TestBase {
 //        app.getGroupHelper().wd.findElement(By.linkText("Logout")).click();
 
 
-
 //        assertEquals(after.size(), before.size() + 1);
 
 //        before.add(group);
@@ -62,5 +73,5 @@ public class GroupCreationTests extends TestBase {
 //        assertEquals(before, after);
 
 
-    }
+}
 
