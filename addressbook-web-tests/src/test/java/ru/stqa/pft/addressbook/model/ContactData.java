9c9f0a0850e0df77816827a3a8579d17918f600f
Perfect User <me@PerfectUser.com>
2020-01-10 10:31:05 -0500

    5.9 - 5.11 Резка и склейка строк

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,9 +1,49 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
-    private final String firstname;
-    private final String secondname;
+
+    private int id;
+    private String firstname;
+    private String secondname;
     private String group;
+    private String homePhone;
+    private String mobilePhone;
+    private String workPhone;
+    private String allPhones;
+
+
+    public String getAllPhones() {
+        return allPhones;
+    }
+
+    public ContactData withAllPhones(String allPhones) {
+        this.allPhones = allPhones;
+        return this;
+    }
+
+
+    public String getWorkPhone() {return workPhone; }
+
+    public ContactData withWorkPhone (String workPhone) {
+        this.workPhone = workPhone;
+        return this;
+    }
+
+    public String getMobilePhone() {return mobilePhone; }
+
+    public ContactData withMobilePhone() {
+        this.mobilePhone = mobilePhone;
+        return this;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public ContactData withHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+        return this;
+    }
 
     public ContactData(String firstname, String secondname, String group) {
         this.firstname = firstname;
