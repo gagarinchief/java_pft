3088c54959555a4ad76c231cfb0353439b4cb3fa
Perfect User <me@PerfectUser.com>
2020-01-14 12:17:37 -0500

    7.4. Модификация тестов
    Использование БД в тестах удаления и создания групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -13,7 +13,7 @@ public class GroupDeletionTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
+        if (app.db().groups().size() == 0) {
             app.group().create(new GroupData().withName("Test 1"));
         }
     }
@@ -21,11 +21,11 @@ public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() throws Exception {
-       Groups before = app.group().all();
+       Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next(); //сперва вызываю iterator(который будет перебирать), потом метод next, который вернет элемент множества
         app.group().delete(deletedGroup);
         assertThat(app.group().count(), equalTo(before.size() - 1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(deletedGroup)) );
 
 
